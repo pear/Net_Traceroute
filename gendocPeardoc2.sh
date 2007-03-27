8f37435327cedac3915145ace3dcbfa9a859656e
Gwynne Raskind <gwynne@php.net>
2007-03-27 14:15:05 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_21_1'.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Traceroute/tags/RELEASE_0_21_1@232739 c90b9560-bf6c-de11-be94-00142212c4b1

--- gendocPeardoc2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-phpdoc -d ./ -i "docs,doc*,example*,generate_package.xml,package.xml,CVS" -t ../peardoc2_Net_Traceroute -o XML:DocBook/peardoc2 -s -p -dn "Net_Traceroute" -dc networking -ti "Net_Traceroute"
